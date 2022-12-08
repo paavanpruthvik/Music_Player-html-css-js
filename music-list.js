054946ea094db39e4efe867b67488a4040ad45b6
Paavan <paavan7777@gmail.com>
2022-12-08 18:18:43 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,26 @@
+let allMusic = [
+    {
+        name: "Heat Waves",
+        artist: "Glass Animals",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Believer",
+        artist: "Imagine Dragons",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Everybody Knows",
+        artist: "Sigrid",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Despacito",
+        artist: "Luis Fonsi",
+        img: "music-4",
+        src: "music-4"
+    },
+];
